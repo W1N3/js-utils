317891296cc7bfd358096f7fa1bf14d638e9a033
W1N3 <gabriel.silva.w1n3@gmail.com>
2023-03-08 16:24:41 -0300

    Tweaks

--- validator.js
+++ validator.js
@@ -1,152 +1,147 @@
-let V = {};
+let validator = {};
 /*
   parameters:
-    type: int
-    acceptedValues:
+    i: int
+    domain:
       raw int: '',
       options of int: [int],
       ranged int: {floor, ceil} (both inclusive)
   returns: true | false
 */
-V.validateInt = function(i, domain) {
+validator.validateInt = function(i, domain) {
   if (typeof i === 'number' &&
       i.toString().indexOf('.') < -1) {
-    if (V
+    if (validator
         .validateObject(domain, ['floor', 'ceil'])) {
       return i >= domain.floor && i <= domain.ceil;
-    } else if (V.validateArray(domain, 'int')) {
+    } else if (validator.validateArray(domain, 'Int')) {
       return domain.includes(i);
     }
-  } else {
-    return false;
   }
+  return false;
 };
 /*
   parameters:
-    type: float
-    acceptedValues:
+    flt: float
+    domain:
       raw float: '',
       options of float: [float],
       ranged float: {floor, ceil} (both inclusive)
   returns: true | false
 */
-V.validateFloat = function(flt, domain) {
-  if (typeof i === 'number') {
-    if (V
+validator.validateFloat = function(flt, domain) {
+  if (typeof flt === 'number') {
+    if (validator
         .validateObject(domain, ['floor', 'ceil'])) {
-      return i >= domain.floor && i <= domain.ceil;
-    } else if (V.validateArray(domain, 'float')) {
-      return domain.includes(i);
+      return flt >= domain.floor && flt <= domain.ceil;
+    } else if (validator.validateArray(domain, 'Float')) {
+      return domain.includes(flt);
     }
-  } else {
-    return false;
   }
+  return false;
 };
 /*
   parameters:
-    type: string
-    acceptedValues:
+    str: string
+    type:
       raw string: '',
       filtered by regex string: 'regex',
       options of string: [string]
   returns: true | false
 */
-V.validateString = function(str, type) {
+validator.validateString = function(str, type) {
   if (typeof str === 'string') {
     if (typeof type === 'string') {
-      return value.match(new RegExp(regex, 'i'));
       if (type.length > 0) {
+        return str.match(new RegExp(type, 'i'));
       } else {
         return true;
       }
-    } else if (V.validateArray(type, 'string')) {
+    } else if (validator.validateArray(type, 'String')) {
       return type.includes(str);
     }
-  } else {
-    return false;
   }
+  return false;
 };
 /*
+    arr: array
+    type:
+      generic array: '',
+      typed array: 'Type'
   parameters:
   returns: true | false
 */
-V.validateArray = function(arr, type) {
+validator.validateArray = function(arr, type) {
   if (Array.isArray(arr)) {
     if (typeof type === 'string' &&
         type.length > 0) {
       arr.every((a) => {
-        if (type === 'int') {
-          return V.validateInt(a, '');
-        } else if (type === 'float') {
-          return V.validateFloat(a, '');
-        } else if (type === 'string') {
-          return V.validateString(a, '');
-        }
-      }); 
+        return validator.validateByType(a, type);
+      });
     } else {
       return true;
     }
   }
+  return false;
 };
 /*
   parameters:
-    type: object
-    acceptedValues:
+    obj: object
+    keys:
       generic object: '',
       structured object: ['keys']
   returns: true | false
 */
-V.validateObject = function(obj, keys) {
+validator.validateObject = function(obj, keys) {
   if (!!obj && typeof obj === 'object') {
-    if(V.validateArray(keys, 'string')) {
-      keys.every((k) => {
+    if (typeof keys === 'string' &&
+        keys.length == 0) {
+      return true;
+    }
+    if (validator.validateArray(keys, 'String')) {
+      return keys.every((k) => {
         return Object.keys(obj).includes(k);
       });
     }
-    return boolCheck;
   }
+  return false;
 };
 /*
   parameters:
-    type: int
+    type: 'Int'
     acceptedValues:
       raw int: '',
       options of int: [int],
       ranged int: {floor, ceil} (both inclusive)
-    type: float
+    type: 'Float'
     acceptedValues:
       raw float: '',
       options of float: [float],
       ranged float: {floor, ceil} (both inclusive)
-    type: string
+    type: 'String'
     acceptedValues:
       raw string: '',
       filtered by regex string: 'regex',
       options of string: [string]
-    type: array
+    type: 'Array'
     acceptedValues:
       generic array: '',
       typed array: 'type'
-    type: object
+    type: 'Object'
     acceptedValues:
       generic object: '',
       structured object: ['keys']
 
   returns:  true | false
 */
-V.genericValidator = function(value, type, acceptedValues = '') {
-  if (type === 'object') {
-    return V.validateObject(value, acceptedValues);
-  } else if (type === 'int') {
-    return V.validateInt(value, acceptedValues);
-  } else if (type === 'float') {
-    return V.validateFloat(value, acceptedValues);
-  } else if (type === 'string') {
-    return V.validateString(value, acceptedValues);
-  } else if (type === 'array') {
-    return V.validateArray(value, acceptedValues);
+validator.validateByType = function(
+  value, type, acceptedValues = '') {
+  if (type === 'ByType') return false;
+  let fn = 'validate' + type;
+  if (fn in validator && typeof validator[fn] === 'function') {
+    return validator[fn](value, acceptedValues);
   }
+  return false;
 };
-return V;
 
-module.exports = V;
+module.exports = validator;
